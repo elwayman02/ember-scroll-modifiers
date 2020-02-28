ba2e033d4913ba1d91d5d9759eae033f9beb7d30
Jordan Hawker <hawker.jordan@gmail.com>
2020-02-28 13:41:37 -0800

    Basic addon-docs setup
    Also, some package.json info filled out

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,13 @@
-import EmberRouter from '@ember/routing/router';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
+export default class Router extends AddonDocsRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
 Router.map(function() {
+  docsRoute(this, function() { /* Your docs routes go here */ });
+
+  this.route('not-found', { path: '/*path' });
 });
