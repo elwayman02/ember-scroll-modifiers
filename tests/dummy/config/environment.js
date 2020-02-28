9d7686429963379bd10367fcd9094934a3d763cb
Jordan Hawker <hawker.jordan@gmail.com>
2020-02-28 14:14:02 -0800

    Fix env config?

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,8 +44,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
     // Allow ember-cli-addon-docs to update the rootURL in compiled assets
-    ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
+    //ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
     // here you can enable a production-specific feature
   }
 
