99eb33d03e2942352326b70564b22be52368d586
Jordan Hawker <hawker.jordan@gmail.com>
2021-01-07 14:13:56 -0800

    Update to 3.24 Blueprint
    Fix linting errors from new prettier config

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'last 2 Safari versions',
 ];
 
 const isCI = Boolean(process.env.CI);
@@ -14,5 +14,5 @@ if (isCI || isProduction) {
 }
 
 module.exports = {
-  browsers
+  browsers,
 };
--- tests/unit/services/observer-manager-test.js
+++ tests/unit/services/observer-manager-test.js
@@ -2,11 +2,13 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
 
-module('Unit | Service | observer-manager', function(hooks) {
+module('Unit | Service | observer-manager', function (hooks) {
   setupTest(hooks);
 
-  hooks.beforeEach(function() {
-    this.observerManager = this.owner.lookup('service:ember-scroll-modifiers@observer-manager');
+  hooks.beforeEach(function () {
+    this.observerManager = this.owner.lookup(
+      'service:ember-scroll-modifiers@observer-manager'
+    );
 
     sinon.stub(this.observerManager._admin, 'observe');
     sinon.stub(this.observerManager._admin, 'unobserve');
@@ -15,32 +17,53 @@ module('Unit | Service | observer-manager', function(hooks) {
 
     this.options = {
       root: undefined,
-    }
+    };
 
     this.callback = () => {};
   });
 
-  test('Creation', function(assert) {
-    assert.ok(this.observerManager._admin, 'intersection-observer-admin is created');
+  test('Creation', function (assert) {
+    assert.ok(
+      this.observerManager._admin,
+      'intersection-observer-admin is created'
+    );
   });
 
-  test('observe', function(assert) {
+  test('observe', function (assert) {
     this.observerManager.observe('foo', this.options);
-    assert.ok(this.observerManager._admin.observe.calledWith('foo', this.options), 'observe is called with correct parameters');
+    assert.ok(
+      this.observerManager._admin.observe.calledWith('foo', this.options),
+      'observe is called with correct parameters'
+    );
   });
 
-  test('unobserve', function(assert) {
+  test('unobserve', function (assert) {
     this.observerManager.unobserve('foo', this.options);
-    assert.ok(this.observerManager._admin.unobserve.calledWith('foo', this.options), 'unobserve is called with correct parameters');
+    assert.ok(
+      this.observerManager._admin.unobserve.calledWith('foo', this.options),
+      'unobserve is called with correct parameters'
+    );
   });
 
-  test('addEnterCallback', function(assert) {
+  test('addEnterCallback', function (assert) {
     this.observerManager.addEnterCallback('foo', this.callback);
-    assert.ok(this.observerManager._admin.addEnterCallback.calledWith('foo', this.callback), 'addEnterCallback is called with correct parameters');
+    assert.ok(
+      this.observerManager._admin.addEnterCallback.calledWith(
+        'foo',
+        this.callback
+      ),
+      'addEnterCallback is called with correct parameters'
+    );
   });
 
-  test('addExitCallback', function(assert) {
+  test('addExitCallback', function (assert) {
     this.observerManager.addExitCallback('foo', this.callback);
-    assert.ok(this.observerManager._admin.addExitCallback.calledWith('foo', this.callback), 'addExitCallback is called with correct parameters');
+    assert.ok(
+      this.observerManager._admin.addExitCallback.calledWith(
+        'foo',
+        this.callback
+      ),
+      'addExitCallback is called with correct parameters'
+    );
   });
 });
