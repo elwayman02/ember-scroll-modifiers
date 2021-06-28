d4c6b46ed18f8d3b250851f5d906155adba2bee1
Jordan Hawker <hawker.jordan@gmail.com>
2021-06-28 11:58:46 -0700

    Ember 3.27 Blueprint Updates
    * Runs ember-cli-update for Ember 3.27
    * [Breaking] Drop Ember 3.16 support (per blueprint)
    * Update GitHub Actions to match new ember-try scenarios
    * Bump yarn version in GitHub action config
    * Add deprecation workflow and silence deprecations
    * Drop IE11 from compile targets
    * Configure test files for qunit eslint plugin

---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+
+/**
+ * This config leverages https://github.com/mixonic/ember-cli-deprecation-workflow
+ * This addon intercepts Ember deprecation warnings and provides an easy way to address the errors.
+ */
+
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  // If we uncomment the line below, any unhandled error i.e a new error introduced would fail the tests.
+  // throwOnUnhandled: true,
+  workflow: [
+    // If we want tests to fail, we need to modify the handler to 'throw'
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'ember.built-in-components.import' },
+    {
+      handler: 'silence',
+      matchId: 'ember.built-in-components.legacy-arguments',
+    },
+    { handler: 'silence', matchId: 'ember.built-in-components.reopen' },
+    {
+      handler: 'silence',
+      matchId: 'ember-glimmer.link-to.positional-arguments',
+    },
+    { handler: 'silence', matchId: 'ember-keyboard.first-responder-inputs' },
+    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+  ],
+};
