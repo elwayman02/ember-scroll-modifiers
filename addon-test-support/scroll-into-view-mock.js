22103ff8720093ff8d92caaf124074764b9f66f1
Abe Adam <abeadam89@gmail.com>
2021-07-02 09:29:54 -0700

    Updating test helper to not use sinon and be more declarative

--- addon-test-support/scroll-into-view-mock.js
+++ addon-test-support/scroll-into-view-mock.js
@@ -1,31 +1,32 @@
-import sinon from 'sinon';
 import { find } from '@ember/test-helpers';
-export default function setupScrollIntoView(hooks) {
-  hooks.beforeEach(function () {
-    this.scrollIntoViewSandbox = sinon.createSandbox();
-    const scrollIntoViewMock = this.scrollIntoViewSandbox.stub();
-    this.scrollIntoViewSandbox.replace(
-      window.Element.prototype,
-      'scrollIntoView',
-      scrollIntoViewMock
-    );
-    // function will check if the passed selector matches any of the elements
-    this.scrollIntoViewCalledWith = (selector) => {
-      let element;
-      // check if it's a string and get the object
-      if (typeof selector === 'string') {
-        element = find(selector);
-      } else {
-        // element was passed in
-        element = selector;
-      }
-      const elementsScrolledTo = scrollIntoViewMock
-        .getCalls()
-        .map((call) => call.thisValue);
-      return elementsScrolledTo.includes(element);
-    };
-  });
-  hooks.afterEach(function () {
-    this.scrollIntoViewSandbox.restore();
-  });
+
+export default function mockScrollIntoView() {
+  let elementsInvokedOn = [];
+  let mockScrollIntoViewFunction = function () {
+    elementsInvokedOn.push(this);
+  };
+  // maually mocking native function
+  let preExistingScrollFunction = window.Element.prototype.scrollIntoView;
+  window.Element.prototype.scrollIntoView = mockScrollIntoViewFunction;
+  // helper fuctions that will be returned
+  let scrollIntoViewCalledWith = (selector) => {
+    let element;
+    // check if it's a string and get the object
+    if (typeof selector === 'string') {
+      element = find(selector);
+    } else {
+      // element was passed in
+      element = selector;
+    }
+    return elementsInvokedOn.includes(element);
+  };
+
+  let resetMock = () => {
+    window.Element.prototype.scrollIntoView = preExistingScrollFunction;
+  };
+
+  return {
+    scrollIntoViewCalledWith,
+    resetMock,
+  };
 }
