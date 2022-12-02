652f63ca8220cd91fde270af058e5328c01378cc
Falk Pauser <falk.pauser@best4tires.com>
2022-12-02 20:23:10 +0100

    Unsilence deprecation warnings for ember-modifier 3.2.x

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -9,11 +9,5 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   // If we uncomment the line below, any unhandled error i.e a new error introduced would fail the tests.
   // throwOnUnhandled: true,
-  workflow: [
-    { handler: 'silence', matchId: 'ember-modifier.function-based-options' },
-    { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
-    { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
-    { handler: 'silence', matchId: 'ember-modifier.use-modify' },
-    { handler: 'silence', matchId: 'this-property-fallback' },
-  ],
+  workflow: [{ handler: 'silence', matchId: 'this-property-fallback' }],
 };
